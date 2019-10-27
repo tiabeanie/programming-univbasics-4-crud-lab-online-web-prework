9bac83c9d9d66d123ea38de8f248830e6c4fc717
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-27 23:12:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,13 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_pets = [gordon, kevin]
+  my_pets << 'gregor'
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  favorite_colors = [blue, purple]
+  favorite_colors.unshift 'green'
 end
 
 def remove_element_from_end_of_array(array)
