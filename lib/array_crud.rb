ec800e14518219d794a04aeca513a33f6e923696
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-27 23:15:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  colors = le[red, orange, blue, green]
+  colors = [red, orange, blue, green]
 end
 
 def add_element_to_end_of_array(array, element)
