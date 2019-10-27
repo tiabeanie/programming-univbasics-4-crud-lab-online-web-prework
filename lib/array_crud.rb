0a94d69ad3c3f5684c174fb18df30fbf4d055bfa
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-27 23:25:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,11 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
    array = ["I", "am", "really", "learning", "arrays!"]
-   array.shift "arrays!"
+   array.pop "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-  
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   array.shift "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
