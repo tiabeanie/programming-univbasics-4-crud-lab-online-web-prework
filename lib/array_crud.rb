fe7b175f601baf22ae1d6c0d894657dd939b76f5
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-27 23:21:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array = ['gordon', 'kevin']
-  add_element_to_end_of_array << 'gregor'
+  
 end
 
 def add_element_to_start_of_array(array, element)
